c1393ff77e90ea60c2414dc8aba3ec1dbb855702
Olaoluwa Oke <63797098+Olaisheretolearn@users.noreply.github.com>
2025-05-09 13:07:37 -0500

    jwt

---
+++ GenNoteApp/src/main/kotlin/com/summerlockin/GenNoteApp/security/JwtService.kt
@@ -0,0 +1,31 @@
+package com.summerlockin.GenNoteApp.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.*
+
+
+@Service
+class JwtService(
+    @Value("JWT_SECRET_BASE64") private val jwtsecret :String
+) {
+    private val secretkey  = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtsecret))
+    private val accessTokenValidityMS = 15L * 60L * 100L
+    val refreshTokenValidityMs = 30L * 24 * 60 * 60 * 100L
+
+    private fun  generateToken(
+        userId: String,
+        type:String,
+        expiry : Long
+    ) :String {
+        return Jwts.builder()
+            .subject(userId)
+            .claim("type", type)
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(secretkey, Jwts.SIG.HS256)
+            .compact()
+    }
+}
